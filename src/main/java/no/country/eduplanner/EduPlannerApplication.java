8a2baaa2eb14009cc22cd85e66ba735de3a3bd87
Fabián Paucar <faibol2000@gmail.com>
2025-01-16 00:35:16 -0500

    login con 4 errores para corregir

--- src/main/java/no/country/eduplanner/EduPlannerApplication.java
+++ src/main/java/no/country/eduplanner/EduPlannerApplication.java
@@ -1,7 +1,17 @@
 package no.country.eduplanner;
 
+import no.country.eduplanner.auth.models.ERole;
+import no.country.eduplanner.auth.models.RoleEntity;
+import no.country.eduplanner.auth.models.UserEntity;
+import no.country.eduplanner.auth.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Set;
 
 @SpringBootApplication
 public class EduPlannerApplication {
@@ -10,4 +20,36 @@ public class EduPlannerApplication {
         SpringApplication.run(EduPlannerApplication.class, args);
     }
 
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Bean
+    CommandLineRunner init(){
+        return args -> {
+
+            UserEntity userEntity = UserEntity.builder()
+                    .email("faibol_men19@hotmail.com")
+                    .username("fievel")
+                    .password(passwordEncoder.encode("1234"))
+                    .roles(Set.of(RoleEntity.builder()
+                            .name(ERole.valueOf(ERole.ADMIN.name()))
+                            .build()))
+                    .build();
+
+            UserEntity userEntity2 = UserEntity.builder()
+                    .email("nena@hotmail.com")
+                    .username("Nena")
+                    .password(passwordEncoder.encode("1234"))
+                    .roles(Set.of(RoleEntity.builder()
+                            .name(ERole.valueOf(ERole.STUDENT.name()))
+                            .build()))
+                    .build();
+
+            userRepository.save(userEntity);
+            userRepository.save(userEntity2);
+        };
+    }
 }
--- src/main/java/no/country/eduplanner/auth/security/SecurityConfig.java
+++ src/main/java/no/country/eduplanner/auth/security/SecurityConfig.java
@@ -1,54 +1,60 @@
 package no.country.eduplanner.auth.security;
 
-import lombok.NoArgsConstructor;
-import org.springframework.cglib.proxy.NoOp;
+import no.country.eduplanner.auth.security.filters.JwtAuthenticationFilter;
+import no.country.eduplanner.auth.security.jwt.JwtUtils;
+import no.country.eduplanner.auth.service.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
+    @Autowired
+    JwtUtils jwtUtils;
+
+    @Autowired
+    UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    JwtAuthenticationFilter authenticationFilter;
+
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity, AuthenticationManager authenticationManager) throws Exception {
+
+        JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(jwtUtils);
+        jwtAuthenticationFilter.setFilterProcessesUrl("/login");
+
         return httpSecurity
                 .csrf(config -> config.disable())
                 .authorizeHttpRequests(auth -> {
-                    auth.requestMatchers("/hello").permitAll();
+                    auth.requestMatchers("/student").permitAll();
+                    auth.requestMatchers("/accessAdmin").hasAnyRole("ADMIN", "STUDENT");
                     auth.anyRequest().authenticated();
                 })
                 .sessionManagement(session -> {
                     session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
                 })
-                .httpBasic(Customizer.withDefaults())
+                .addFilter(jwtAuthenticationFilter)
+                .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
-    @Bean
-    UserDetailsService userDetailsService() {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User.withUsername("fabian")
-                               .password("1234")
-                               .roles("ADMIN")
-                               .build());
-        return manager;
-    }
 
     @Bean
     AuthenticationManager authenticationManager(HttpSecurity httpSecurity, PasswordEncoder passwordEncoder) throws Exception {
         return  httpSecurity.getSharedObject(AuthenticationManagerBuilder.class)
-                .userDetailsService(userDetailsService())
+                .userDetailsService(userDetailsService)
                 .passwordEncoder(passwordEncoder)
                 .and().build();
     }
