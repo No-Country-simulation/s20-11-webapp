2ac0969d76dd27b1ca5c28eb0623fce80c474a8e
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2025-01-17 14:43:52 -0300

    move routing out of the app component. Replace with landing placeholder

--- Frontend/eduPlanner/src/App.jsx
+++ Frontend/eduPlanner/src/App.jsx
@@ -1,61 +1,8 @@
-import { useState } from 'react'
 
-import './index.css'
-import './App.css'
-import Login from './Components/Login'
-import { BrowserRouter, Routes, Route, Navigate, redirect } from "react-router-dom"
-import HomeStudent from './Components/HomeStudent'
-import HomeCoordinator from './Components/HomeCoordinator'
-import Calendario from './Components/Calendario'
+import "./App.css";
+import "./index.css";
 
-function App() {
-  
-
-  return (
-    <><BrowserRouter>
-      <Routes>
-    
-        {
-          <Route
-            path="/login" element={
-              <div>                
-                <Login />
-              </div>
-            }
-          />
-        }
-        {
-          <Route
-            path="/homestudent" element={
-              <div>                
-                <HomeStudent/>
-              </div>
-            }
-          />
-        } 
-        {
-          <Route
-            path="/homecoordinator" element={
-              <div>                
-                <HomeCoordinator/>
-              </div>
-            }
-          />
-        }
-        {
-          <Route
-            path="/calendar" element={
-              <div>                
-                <Calendario/>
-              </div>
-            }
-          />
-        }         
-    </Routes>
-  </BrowserRouter>
-      
-    </>
-  )
+export default function App() {
+  return <div>Landing Page</div>;
 }
 
-export default App
