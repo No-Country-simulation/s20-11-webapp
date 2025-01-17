cc930701335c2e067b2f83c8019be78cf3900ee7
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2025-01-17 20:18:31 -0300

    decrease padding

--- Frontend/eduPlanner/src/Components/title-bar.jsx
+++ Frontend/eduPlanner/src/Components/title-bar.jsx
@@ -4,13 +4,15 @@ import { Link } from "react-router-dom";
 
 export function TitleBar({ title }) {
   return (
-    <div className="mx-auto sm:mx-0 flex flex-col  sm:flex-row items-center gap-4  bg-muted/50 rounded shadow-sm border p-1 w-fit sm:pr-6">
+    <div className="mx-auto sm:mx-0 flex flex-col  sm:flex-row items-center gap-1 sm:gap-4  bg-muted/50 rounded shadow-sm border p-1 w-fit sm:pr-6">
       <Link to=".." relative="path" viewTransition prefetch="intent">
         <Button variant="outline" size="icon" className="">
           <ArrowLeft />
         </Button>
       </Link>
-      <div className="text-lg sm:text-xl sm:font-bold text-nowrap ">{title}</div>
+      <div className="text-lg sm:text-xl sm:font-bold text-nowrap ">
+        {title}
+      </div>
     </div>
   );
 }
