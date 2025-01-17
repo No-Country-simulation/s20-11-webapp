642d568b57d1b8a26f27891d315fcd4dd0a64bee
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2025-01-17 15:09:01 -0300

    improve styling

--- Frontend/eduPlanner/src/Components/title-bar.jsx
+++ Frontend/eduPlanner/src/Components/title-bar.jsx
@@ -4,15 +4,13 @@ import { Link } from "react-router-dom";
 
 export function TitleBar({ title }) {
   return (
-    <div className="flex items-center gap-4  ">
-    <Link to=".." relative="path" viewTransition prefetch="intent">
-      <Button variant="outline" size="icon">
-        <ArrowLeft />
-      </Button>
-    </Link>
-    <div className="text-xl font-bold">
-        {title}
-      </div>
+    <div className="flex items-center gap-4  bg-secondary rounded shadow-sm border p-1 w-fit pr-6">
+      <Link to=".." relative="path" viewTransition prefetch="intent">
+        <Button variant="outline" size="icon">
+          <ArrowLeft />
+        </Button>
+      </Link>
+      <div className="text-xl font-bold">{title}</div>
     </div>
   );
 }
