4fbda5ccdf6ce2dae0126a5892456a67871cf899
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2025-01-17 21:20:52 -0300

    remove unreachable return

--- Frontend/eduPlanner/src/Components/layout/layout.jsx
+++ Frontend/eduPlanner/src/Components/layout/layout.jsx
@@ -4,23 +4,10 @@ import Footer from "./Footer";
 import Header from "./Header";
 
 export default function Layout() {
-
   return (
     <>
       <Header />
       <main className="min-h-[calc(100vh-75px)] bg-gradient-to-b from-background via-secondary/30 to-secondary/10">
-        <div className="container mx-auto px-4 py-8 relative z-10">
-          <Outlet />
-        </div>
-      </main>
-      <Footer />
-      <ScrollRestoration />
-    </>
-  );
-  return (
-    <>
-      <Header />
-      <main className="min-h-[calc(100vh-75px)] bg-background">
         <div className="container mx-auto px-4 py-8 relative z-10">
           <Outlet />
         </div>
