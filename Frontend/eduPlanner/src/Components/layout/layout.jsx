e767b5f6b49a98f050373eceff36c93e0f247b3b
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2025-01-18 13:04:30 -0300

    update padding

--- Frontend/eduPlanner/src/Components/layout/layout.jsx
+++ Frontend/eduPlanner/src/Components/layout/layout.jsx
@@ -8,7 +8,7 @@ export default function Layout() {
     <>
       <Header />
       <main className="min-h-[calc(100vh-75px)] bg-gradient-to-b from-background via-secondary/30 to-secondary/10">
-        <div className="container mx-auto px-4 py-8 relative z-10">
+        <div className="container mx-auto px-4 py-6 relative z-10">
           <Outlet />
         </div>
       </main>
