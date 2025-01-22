d69c8e0c769c54d4c90d37c0734059b30755c7cd
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2025-01-22 16:02:54 -0300

    Add method to check course existence by user ID and retrieve courses created by a specific user.

--- src/main/java/no/country/eduplanner/courses/infra/persistence/CourseRepository.java
+++ src/main/java/no/country/eduplanner/courses/infra/persistence/CourseRepository.java
@@ -4,10 +4,15 @@ import no.country.eduplanner.courses.domain.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
 
     @Query("SELECT c FROM Course c LEFT JOIN FETCH c.classDays WHERE c.id = :id")
     Optional<Course> findByIdWithClassDays(Long id);
+
+    boolean existsByIdAndCourseUsers_UserId(Long courseId, Long userId);
+
+    List<Course> findByCreatedByUserId(Long adminId);
 }
