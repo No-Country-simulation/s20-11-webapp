0b11e56421afd00a590d26836283dc00141ecc57
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2025-01-22 12:52:31 -0300

    Revamp security configuration: - Remove unnecessary annotation at class level. - Remove unnecessary dependencies. - Remove bean for UserDetailsService. Implementation moved to its own class. - Remove unused method authenticationProvider() - Remake security filter chain, include logout handler.

--- src/main/java/no/country/eduplanner/auth/security/SecurityConfig.java
+++ src/main/java/no/country/eduplanner/auth/security/SecurityConfig.java
@@ -1,95 +1,56 @@
 package no.country.eduplanner.auth.security;
 
-import no.country.eduplanner.auth.models.UserEntity;
-import no.country.eduplanner.auth.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import no.country.eduplanner.auth.security.filters.JwtFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import java.util.Collection;
-
 @Configuration
-@EnableWebSecurity
 @EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-    private UserRepository userRepository;
+    private final JwtFilter jwtFilter;
 
-    @Autowired
-    private SecurityFilter securityFilter;
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity, AuthenticationManager authenticationManager) throws Exception {
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 
         return httpSecurity
-                .csrf(csrf -> csrf.disable())
-                .httpBasic(Customizer.withDefaults())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/login", "/auth/register").permitAll()
-                        .requestMatchers("/auth/admin").hasRole("ADMIN")
-                        .requestMatchers("/auth/estudiante").hasAnyRole("ADMIN", "STUDENT")
-                        .anyRequest().authenticated())
-
-                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers(HttpMethod.POST, "/auth/**").permitAll()
+                        .requestMatchers("/accessAdmin").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> {
+                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                })
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .logout(logout -> logout
+                        .logoutUrl("/auth/logout")
+                        .logoutSuccessHandler((req, res, auth) -> SecurityContextHolder.clearContext()))
                 .build();
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        return username -> {
-            // Buscar el usuario en la base de datos
-
-            UserEntity userEntity = userRepository.findByUsername(username)
-                    .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado con el nombre de usuario: " + username));
-
-            // Convertir los roles en autoridades para Spring Security
-            Collection<? extends GrantedAuthority> authorities = userEntity.getRoles().stream()
-                    .map(roleEntity -> new SimpleGrantedAuthority("ROLE_" + roleEntity.getERole().name()))
-                    .toList();
-
-            // Retornar el usuario con su nombre, contraseña y roles
-            return new org.springframework.security.core.userdetails.User(
-                    userEntity.getUsername(),
-                    userEntity.getPassword(),
-                    authorities
-            );
-        };
-    }
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration ) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
-
-    public AuthenticationProvider authenticationProvider(){
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setPasswordEncoder(passwordEncoder());
-        provider.setUserDetailsService(userDetailsService());
-        return provider;
-    }
-
-
-
-
-    @Bean
-    public  PasswordEncoder passwordEncoder(){
-
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
