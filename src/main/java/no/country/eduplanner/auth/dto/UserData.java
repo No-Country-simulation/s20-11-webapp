a77527dc99b52e498d0c9d2d1b23791a28d7b919
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2025-01-22 16:00:41 -0300

    Enhance UserData DTO to include user roles

--- src/main/java/no/country/eduplanner/auth/dto/UserData.java
+++ src/main/java/no/country/eduplanner/auth/dto/UserData.java
@@ -1,8 +1,13 @@
 package no.country.eduplanner.auth.dto;
 
+import no.country.eduplanner.auth.models.UserRole;
+
+import java.util.Set;
+
 public record UserData(
         Long id,
         String email,
+        Set<UserRole> roles,
         boolean isProfileComplete,
         String firstName,
         String lastName,
