a36f680794dfcb99ef8cb222a1b4e0c251d80978
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2025-01-22 12:55:48 -0300

    Fix JWT filter: - Remove dependencies to user repository. - Avoid unnecessary db calls when validating user token. - Add token validation.

---
+++ src/main/java/no/country/eduplanner/auth/security/filters/JwtFilter.java
@@ -0,0 +1,83 @@
+package no.country.eduplanner.auth.security.filters;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import no.country.eduplanner.auth.security.jwt.JwtTokenService;
+import no.country.eduplanner.auth.security.jwt.JwtUtils;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtTokenService jwtService;
+    private final JwtUtils jwtUtils;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws SecurityException, IOException, ServletException {
+
+        try {
+
+            if (request.getRequestURI().startsWith("/auth")) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+
+            String jwtToken = jwtUtils.extractToken(request);
+
+            if (jwtToken == null) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+
+            processToken(jwtToken, request);
+
+        } catch (Exception e) {
+            log.error("🚫 Authentication error", e);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private void processToken(String token, HttpServletRequest request) {
+        String userEmail = jwtService.extractUserEmail(token);
+
+        if (userEmail == null || SecurityContextHolder.getContext().getAuthentication() != null) {
+            return;
+        }
+
+        if (!jwtService.isTokenValid(token)) {
+            return;
+        }
+
+        UserDetails userDetails = jwtService.buildUserDetailsFromToken(token);
+        authenticateUser(userDetails, request);
+
+    }
+
+    private void authenticateUser(UserDetails userDetails, HttpServletRequest request) {
+        var authToken = new UsernamePasswordAuthenticationToken(
+                userDetails,
+                null,
+                userDetails.getAuthorities()
+        );
+        authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+    }
+
+}
+
