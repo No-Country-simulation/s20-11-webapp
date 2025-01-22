e1cfc575d2adfa1b01352bdf1b9784da953e4550
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2025-01-22 16:01:58 -0300

    Implement role based restriction at course level

--- src/main/java/no/country/eduplanner/courses/application/service/CourseAccessService.java
+++ src/main/java/no/country/eduplanner/courses/application/service/CourseAccessService.java
@@ -1,9 +1,13 @@
 package no.country.eduplanner.courses.application.service;
 
 import lombok.RequiredArgsConstructor;
+import no.country.eduplanner.auth.dto.UserData;
+import no.country.eduplanner.auth.models.UserRole;
+import no.country.eduplanner.auth.services.UserDataService;
 import no.country.eduplanner.courses.application.exception.CourseNotFoundException;
 import no.country.eduplanner.courses.domain.entity.Course;
 import no.country.eduplanner.courses.infra.persistence.CourseRepository;
+import no.country.eduplanner.shared.application.exception.UnauthorizedAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,14 +17,14 @@ import org.springframework.transaction.annotation.Transactional;
 public class CourseAccessService {
 
     private final CourseRepository courseRepository;
+    private final UserDataService userDataService;
 
 
     public Course getCourseWithAccessCheck(Long courseId) {
         Course course = courseRepository.findById(courseId)
                                         .orElseThrow(() -> new CourseNotFoundException(courseId));
 
-        // TODO: Uncomment when auth is done
-        // verifyUserHasAccessToCourse(course);
+        verifyUserHasAccessToCourse(course);
 
         return course;
     }
@@ -29,20 +33,28 @@ public class CourseAccessService {
         Course course = courseRepository.findByIdWithClassDays(courseId)
                                         .orElseThrow(() -> new CourseNotFoundException(courseId));
 
-        // TODO: Uncomment when auth is done
-        // verifyUserHasAccessToCourse(course);
+        verifyUserHasAccessToCourse(course);
 
         return course;
     }
 
 
-    // TODO: ADD LATER: 👇
-    /*
     private void verifyUserHasAccessToCourse(Course course) {
-        User currentUser = getCurrentAuthenticatedUser();
-        if (!course.getUser().getId().equals(currentUser.getId())) {
-            throw new UnauthorizedAccessException("No tienes acceso a este curso");
+        UserData currentUserData = userDataService.getCurrentUserData();
+
+        //Admin tienen acceso a los cursos que ha creado
+        if (currentUserData.roles().contains(UserRole.ADMIN)) {
+            if (!course.getCreatedByUserId().equals(currentUserData.id())) {
+                throw new UnauthorizedAccessException("No tienes acceso a este curso");
+            }
+        } else {
+            //Estudiante solo tienen acceso a su curso asignado
+            boolean hasAccess = courseRepository.existsByIdAndCourseUsers_UserId(course.getId(), currentUserData.id());
+            if (!hasAccess) {
+                throw new UnauthorizedAccessException("No tienes acceso a este curso");
+            }
         }
     }
-    */
+
+
 }
