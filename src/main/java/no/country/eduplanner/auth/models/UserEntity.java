aafaa3cff96be87d468034985b6548741204e2dc
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2025-01-22 12:33:48 -0300

    Simplify UserEntity: - Removed fields non-related to application domain (username). - Removed implementation of UserDetails in JPA entity. Should not mix persistence logic with authentication. - Changed relation towards user roles to a CollectionTable.

--- src/main/java/no/country/eduplanner/auth/models/UserEntity.java
+++ src/main/java/no/country/eduplanner/auth/models/UserEntity.java
@@ -1,70 +1,31 @@
 package no.country.eduplanner.auth.models;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.*;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
+import lombok.experimental.SuperBuilder;
+import no.country.eduplanner.shared.domain.base.BaseEntity;
 
-import java.util.Collection;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
+@SuperBuilder
 @Entity
 @Table(name = "users")
-public class UserEntity implements UserDetails {
+public class UserEntity extends BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @NotBlank
-    @Size(max = 30)
-    @Column(unique = true)
-    private String username;
-
-    @NotBlank
-    private String password;
-
-    @Email
-    @NotBlank
-    @Size(max=80)
     private String email;
 
-    @ManyToMany(fetch = FetchType.EAGER, targetEntity = RoleEntity.class, cascade = CascadeType.ALL)
-    @JoinTable(name = "user_roles",
-            joinColumns= @JoinColumn(name = "user_id"),
-            inverseJoinColumns = @JoinColumn(name = "role_id"))
-    private Set<RoleEntity> roles= new HashSet<>();
+    private String password;
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return roles.stream()
-                .map(roleEntity -> new SimpleGrantedAuthority("ROLE_" + roleEntity.getERole().name()))
-                .toList();
-    }
+    @Builder.Default
+    @Enumerated(EnumType.STRING)
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "role", nullable = false)
+    private Set<UserRole> roles = new HashSet<>();
 
-    @Override
-    public boolean isAccountNonExpired() {
-        return UserDetails.super.isAccountNonExpired();
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return UserDetails.super.isAccountNonLocked();
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return UserDetails.super.isCredentialsNonExpired();
-    }
 }
