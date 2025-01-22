0c16c1d675c069632431b85bb420dc6877a2217c
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2025-01-22 12:37:25 -0300

    Remove unnecessary repository for roles and unused methods.

--- src/main/java/no/country/eduplanner/auth/repository/UserRepository.java
+++ src/main/java/no/country/eduplanner/auth/repository/UserRepository.java
@@ -5,10 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository <UserEntity, Long>{
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
     Optional<UserEntity> findByEmail(String email);
-    Optional<UserEntity> findByUsername(String username);
-    boolean existsByUsername(String username);
+
     boolean existsByEmail(String email);
 
 }
