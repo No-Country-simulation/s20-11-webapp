45e671458fd5295469c515581149381f12d1ef4b
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2025-01-22 13:30:26 -0300

    Add class to represent account profile info

---
+++ src/main/java/no/country/eduplanner/auth/models/Profile.java
@@ -0,0 +1,36 @@
+package no.country.eduplanner.auth.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import no.country.eduplanner.shared.domain.vo.Image;
+import org.springframework.util.StringUtils;
+
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Setter
+public class Profile {
+
+    @Column(name = "first_name")
+    @Size(max = 50)
+    private String firstName;
+
+    @Column(name = "last_name")
+    @Size(max = 50)
+    private String lastName;
+
+    @Column(name = "profile_photo")
+    private Image photo;
+
+    public boolean isProfileComplete() {
+        return StringUtils.hasText(firstName) &&
+               StringUtils.hasText(lastName) &&
+               photo != null;
+    }
+
+}
