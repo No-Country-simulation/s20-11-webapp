ac2eb30048f79788e07aec2fa150c64d1fa33bf8
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2025-01-23 13:36:19 -0300

    Remove classes

--- src/main/java/no/country/eduplanner/shared/application/events/NotificationEvent.java
+++
@@ -1,9 +0,0 @@
-package no.country.eduplanner.shared.application.events;
-
-import java.time.LocalDateTime;
-
-public record NotificationEvent(
-        String message,
-        LocalDateTime at
-) {
-}
