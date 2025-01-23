0d5bfd6c4772306676756efba532901ddfaeb0ea
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2025-01-23 13:55:33 -0300

    add async configuration for async events, fixing the security context not propagating properly

---
+++ src/main/java/no/country/eduplanner/config/AsyncConfig.java
@@ -0,0 +1,43 @@
+package no.country.eduplanner.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskDecorator;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig implements AsyncConfigurer {
+
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(4);
+        executor.setQueueCapacity(50);
+        executor.setThreadNamePrefix("AsyncThread-");
+        executor.setTaskDecorator(new SecurityContextCopyingDecorator());
+        executor.initialize();
+        return executor;
+    }
+
+    static class SecurityContextCopyingDecorator implements TaskDecorator {
+        @Override
+        public Runnable decorate(Runnable runnable) {
+            SecurityContext context = SecurityContextHolder.getContext();
+            return () -> {
+                try {
+                    SecurityContextHolder.setContext(context);
+                    runnable.run();
+                } finally {
+                    SecurityContextHolder.clearContext();
+                }
+            };
+        }
+    }
+}
