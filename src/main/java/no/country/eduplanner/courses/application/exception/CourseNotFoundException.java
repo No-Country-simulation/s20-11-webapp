c797616247ed90f4f22f2f08bacd055f780adc80
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2025-01-24 22:51:49 -0300

    update exceptions

--- src/main/java/no/country/eduplanner/courses/application/exception/CourseNotFoundException.java
+++ src/main/java/no/country/eduplanner/courses/application/exception/CourseNotFoundException.java
@@ -5,10 +5,7 @@ import org.springframework.http.HttpStatus;
 public class CourseNotFoundException extends CourseException {
 
     public CourseNotFoundException(Long courseId) {
-        super("No existe curso con ID: [%d]".formatted(courseId), HttpStatus.NOT_FOUND);
+        super("No existe curso con ID: [%d]".formatted(courseId), HttpStatus.NOT_FOUND, "COURSE_NOT_FOUND");
     }
 
-    public CourseNotFoundException(String message) {
-        super(message, HttpStatus.NOT_FOUND);
-    }
 }
