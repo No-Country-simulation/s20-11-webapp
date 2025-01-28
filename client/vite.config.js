77f09117694fe43eb25003ecec651a2017106a1a
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2025-01-28 16:23:30 -0300

    update vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,19 @@
-import { fileURLToPath } from 'url';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 // Define __dirname manualmente
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true, // asegura que Vite escuche en 0.0.0.0
+
+    allowedHosts: ['educplanner.onrender.com'], 
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'), // Define '@' como la carpeta 'src'
