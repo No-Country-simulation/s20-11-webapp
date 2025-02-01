238e085957fd67ebbfd53f932b6deecf71b7e00a
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2025-02-01 16:10:52 -0300

    set timezone for jvm

--- src/main/java/no/country/eduplanner/EduPlannerApplication.java
+++ src/main/java/no/country/eduplanner/EduPlannerApplication.java
@@ -3,10 +3,13 @@ package no.country.eduplanner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class EduPlannerApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Argentina/Buenos_Aires"));
         SpringApplication.run(EduPlannerApplication.class, args);
     }
 
