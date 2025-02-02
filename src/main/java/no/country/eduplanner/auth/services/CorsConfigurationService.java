58dda6a190a8a1ef7d4b3f6c6b847e680ea639cd
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2025-02-01 22:06:39 -0300

    prepare cors configuration for prod

--- src/main/java/no/country/eduplanner/auth/services/CorsConfigurationService.java
+++ src/main/java/no/country/eduplanner/auth/services/CorsConfigurationService.java
@@ -1,26 +1,41 @@
 package no.country.eduplanner.auth.services;
 
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Service
+@Setter
+@ConfigurationProperties(prefix = "app.security.cors")
 public class CorsConfigurationService implements CorsConfigurationSource {
+
+    private String allowedOrigins;
 
 
     @Override
     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOriginPatterns(List.of("*"));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Include OPTIONS
+
+        if (!StringUtils.hasText(allowedOrigins)) {
+            throw new IllegalArgumentException("CORS_ALLOWED_ORIGINS is not properly configured");
+        }
+        List<String> origins = Arrays.asList(allowedOrigins.split(","));
+        config.setAllowedOrigins(origins);
+
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("Authorization"));
         config.setAllowCredentials(true);
+
         return config;
 
     }
@@ -30,28 +45,3 @@ public class CorsConfigurationService implements CorsConfigurationSource {
     }
 
 }
-
-//TODO: PROD
-//@Service
-//@Setter
-//@ConfigurationProperties(prefix = "app.security.cors")
-//public class CorsConfigurationService implements CorsConfigurationSource {
-//
-//    private List<String> allowedOrigins;
-//
-//    @Override
-//    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-//        CorsConfiguration config = new CorsConfiguration();
-//        config.setAllowedOrigins(allowedOrigins);
-//        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
-//        config.setAllowedHeaders(List.of("*"));
-//        config.setExposedHeaders(List.of("Authorization"));
-//        return config;
-//
-//    }
-//
-//    public void configure(CorsConfigurer<HttpSecurity> cors) {
-//        cors.configurationSource(this);
-//    }
-//
-//}
